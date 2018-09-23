726b5017bdf491a6fe38721452d887f887e5505c
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2018-09-23 16:24:11 +0100

    reuse existing man toolwindow (otherwise editor sometimes loses focus)

--- src/manpageviewer/OpenManPage.kt
+++ src/manpageviewer/OpenManPage.kt
@@ -1,6 +1,7 @@
 package manpageviewer
 
 import com.intellij.execution.filters.TextConsoleBuilderFactory
+import com.intellij.execution.ui.ConsoleView
 import com.intellij.execution.ui.ConsoleViewContentType.NORMAL_OUTPUT
 import com.intellij.execution.ui.ExecutionConsole
 import com.intellij.icons.AllIcons
@@ -10,7 +11,10 @@ import com.intellij.openapi.editor.Editor
 import com.intellij.openapi.fileEditor.FileEditorManager
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.util.Disposer
+import com.intellij.openapi.util.Key
+import com.intellij.openapi.wm.ToolWindow
 import com.intellij.openapi.wm.ToolWindowAnchor
+import com.intellij.openapi.wm.ToolWindowManager
 import java.awt.BorderLayout
 import java.io.InputStreamReader
 import javax.swing.JPanel
@@ -56,25 +60,41 @@ class OpenManPage : AnAction() {
 
     private data class ExecResult(val exitCode: Int, val stdout: String, val stderr: String)
 
+    private val consoleKey: Key<ConsoleView> = Key("manpageviewer.consoleKey")
+    private val toolWindowId = "Man"
+
     private fun showInToolWindow(message: String, consoleTitle: String, project: Project) {
+        val existingConsole = project.getUserData(consoleKey)
+        val existingToolWindow = ToolWindowManager.getInstance(project).getToolWindow(toolWindowId)
+        if (existingConsole != null && existingToolWindow != null) {
+            existingConsole.clear()
+            existingConsole.print(message, NORMAL_OUTPUT)
+            existingConsole.scrollTo(0)
+            existingToolWindow.title = consoleTitle
+            existingToolWindow.activate({}, false)
+            return
+        }
+
         val console = TextConsoleBuilderFactory.getInstance().createBuilder(project).console
-        console.print(message, NORMAL_OUTPUT)
+        project.putUserData(consoleKey, console)
 
         val toolWindowDisposable = project.newChildDisposable()
         Disposer.register(toolWindowDisposable, console)
         val toolbarActions = DefaultActionGroup().apply {
             add(object : AnAction(Cancel) {
                 override fun actionPerformed(event: AnActionEvent) {
+                    project.putUserData(consoleKey, null)
                     Disposer.dispose(toolWindowDisposable)
                 }
             })
         }
         val consoleComponent = MyConsolePanel(console, toolbarActions)
+        console.print(message, NORMAL_OUTPUT)
         console.scrollTo(0)
 
         val toolWindow = registerToolWindowIn(
             project = project,
-            toolWindowId = "man",
+            toolWindowId = toolWindowId,
             parentDisposable = toolWindowDisposable,
             location = ToolWindowAnchor.BOTTOM,
             toolbarActionGroup = null,
@@ -82,6 +102,7 @@ class OpenManPage : AnAction() {
         )
         toolWindow.title = consoleTitle
         toolWindow.icon = AllIcons.Toolwindows.Documentation // Has to be 13x13 icon
+        toolWindow.activate({}, false)
     }
 
     private class MyConsolePanel(console: ExecutionConsole, toolbarActions: ActionGroup) : JPanel(BorderLayout()) {
